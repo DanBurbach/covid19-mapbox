f37bf9b2eb41f81b3954f2d1ed416fea1d7a7146
DanBurbach <dburbach1982@gmail.com>
2020-04-16 22:43:26 -0700

    wrap Routes with BrowserRouter, add Error404 if not entered in correct route, generate a Main page that renders a MapBox using profile login token, link to assets Main.scss

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import useSWR from "swr"; // React hook to fetch the data
+import lookup from "country-code-lookup"; // npm module to get ISO Code for countries
+
+import "../../assets/Main.scss";
+
+// Mapbox css - needed to make tooltips work later in this article
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGJ1cmJhY2gxOTgyIiwiYSI6ImNrNjhhbXNwbzAzMWczcG56azQ2anhlcmsifQ.oIeM3Zzm_nFsu-dbACDbZg";
+
+function Main() {
+  const mapboxElRef = useRef(null); // DOM element to render map
+
+  // Initialize our map
+  useEffect(() => {
+    // You can store the map instance with useRef too
+    const map = new mapboxgl.Map({
+      container: mapboxElRef.current,
+      style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
+      center: [16, 27], // initial geo location
+      zoom: 2, // initial zoom
+    });
+
+    // Add navigation controls to the top right of the canvas
+    map.addControl(new mapboxgl.NavigationControl());
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="mapContainer">
+        {/* Assigned Mapbox container */}
+        <div className="mapBox" ref={mapboxElRef} />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
