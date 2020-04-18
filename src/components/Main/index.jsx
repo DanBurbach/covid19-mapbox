4314de05f70ee540c8ff7b0dd8afdc4ba3eb597a
DanBurbach <dburbach1982@gmail.com>
2020-04-17 21:10:53 -0700

    add on mouseover call to provide cases, deaths, country data, and if it is a province in a pop-up. Also provide a flag based off of countryISO. When mouse leave, reset data and remove popup

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -94,14 +94,57 @@ function Main() {
             ],
           }
         });
+
+        const popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+        });
+
+        let lastId;
+
+        map.on('mousemove', 'circles', (e) => {
+            const id = e.features[0].properties.id;
+            if (id !== lastId) {
+                lastId = id;
+
+                const { cases, deaths, country, province} = e.features[0].properties;
+
+                map.getCanvas().style.cursor = 'pointer';
+
+                const coordinates = e.features[0].geometry.coordinates.slice();
+
+                const countryISO = lookup.byCountry(country)?.iso2 || lookup.byInternet(country)?.iso2;
+
+                const provinceHTML = province !== 'null' ? `<p>Province: <b>${province}</b></p>` : '';
+
+                const mortalityRate = ((deaths / cases) * 100).toFixed(2);
+
+                const countryFlagHTML = Boolean(countryISO) 
+                    ? `<img src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>` 
+                    : '';
+
+                const HTML = `<p>Country: <b>${country}</b></p>
+                        ${provinceHTML}
+                        <p>Cases: <b>${cases}</b></p>
+                        <p>Deaths: <b>${deaths}</b></p>
+                        <p>Mortality Rate: <b>${mortalityRate}%</b></p>
+                        ${countryFlagHTML}`;
+
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                    }
+                popup.setLngLat(coordinates).setHTML(HTML).addTo(map);
+                }
+            });
+
+            map.on("mouseleave", "circles", function() {
+                lastId = undefined;
+                map.getCanvas().style.cursor = "";
+                popup.remove();
+            });
       });
     }
   }, [data]);
-
-
-//     // Add navigation controls to the top right of the canvas
-//     map.addControl(new mapboxgl.NavigationControl());
-//   }, []);
 
   return (
     <div className='App'>
