7707a85e8458f08a6dfdc51d12c463b16c5bbead
DanBurbach <dburbach1982@gmail.com>
2020-04-17 20:17:43 -0700

    use data-driven-styling in the paint property of the circle-radius to change map sizing based off of interpolate data of amount of cases and circle-radius

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,15 +1,15 @@
-import React, { useRef, useEffect } from "react";
-import mapboxgl from "mapbox-gl";
-import useSWR from "swr"; // React hook to fetch the data
-import lookup from "country-code-lookup"; // npm module to get ISO Code for countries
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import useSWR from 'swr'; // React hook to fetch the data
+import lookup from 'country-code-lookup'; // npm module to get ISO Code for countries
 
-import "../../assets/Main.scss";
+import '../../assets/Main.scss';
 
 // Mapbox css - needed to make tooltips work later in this article
-import "mapbox-gl/dist/mapbox-gl.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiZGJ1cmJhY2gxOTgyIiwiYSI6ImNrNjhhbXNwbzAzMWczcG56azQ2anhlcmsifQ.oIeM3Zzm_nFsu-dbACDbZg";
+  'pk.eyJ1IjoiZGJ1cmJhY2gxOTgyIiwiYSI6ImNrNjhhbXNwbzAzMWczcG56azQ2anhlcmsifQ.oIeM3Zzm_nFsu-dbACDbZg';
 
 function Main() {
     const mapboxElRef = useRef(null); // DOM element to render map
@@ -19,16 +19,16 @@ function Main() {
         .then((r) => r.json())
         .then((data) =>
         data.map((point, index) => ({
-            type: "Feature",
+            type: 'Feature',
             geometry: {
-            type: "Point",
+            type: 'Point',
             coordinates: [
                 point.coordinates.longitude,
                 point.coordinates.latitude,
             ],
             },
             properties: {
-            id: index, // unique identifier in this case the index
+            id: index,
             country: point.country,
             province: point.province,
             cases: point.stats.confirmed,
@@ -37,42 +37,50 @@ function Main() {
         }))
     );
 
-    const { data } = useSWR("https://corona.lmao.ninja/v2/jhucsse", fetcher);
+    const { data } = useSWR('https://corona.lmao.ninja/v2/jhucsse', fetcher);
 
   // Initialize our map
     useEffect(() => {
-        // You can store the map instance with useRef too
     if (data) {
         const map = new mapboxgl.Map({
             container: mapboxElRef.current,
-            style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
-            center: [16, 27], // initial geo location
-            zoom: 2, // initial zoom
+            style: 'mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k',
+            center: [16, 27],
+            zoom: 2,
         });
 
       // Call this method when the map is loaded
-      map.once("load", function() {
+      map.once('load', function() {
         // Add our SOURCE
-        // with id "points"
-        map.addSource("points", {
-          type: "geojson",
+        // with id 'points'
+        map.addSource('points', {
+          type: 'geojson',
           data: {
-            type: "FeatureCollection",
+            type: 'FeatureCollection',
             features: data
           }
         });
         map.addControl(new mapboxgl.NavigationControl());
         // Add our layer
         map.addLayer({
-          id: "circles",
-          source: "points", // this should be the id of the source
-          type: "circle",
-          // paint properties
+          id: 'circles',
+          source: 'points', // this should be the id of the source
+          type: 'circle',
           paint: {
-            "circle-opacity": 0.75,
-            "circle-stroke-width": 1,
-            "circle-radius": 4,
-            "circle-color": "#FFEB3B"
+            'circle-opacity': 0.75,
+            'circle-stroke-width': 1,
+            'circle-color': '#FFEB3B',
+            'circle-radius': [
+              'interpolate',
+              ['linear'],
+              ['get', 'cases'],
+              1, 4,
+              1000, 8,
+              4000, 10,
+              8000, 14,
+              12000, 18,
+              100000, 40
+            ],
           }
         });
       });
@@ -85,10 +93,10 @@ function Main() {
 //   }, []);
 
   return (
-    <div className="App">
-      <div className="mapContainer">
+    <div className='App'>
+      <div className='mapContainer'>
         {/* Assigned Mapbox container */}
-        <div className="mapBox" ref={mapboxElRef} />
+        <div className='mapBox' ref={mapboxElRef} />
       </div>
     </div>
   );
