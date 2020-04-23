be30a56c41f24ba4e0502efed2e0c546811a86ed
DanBurbach <dburbach1982@gmail.com>
2020-04-22 23:25:16 -0700

    update Covid19_ticker to be a Class component for funtionality, work towards making an API call to newspai.org for news source, and render in temporary placeholder ticker at bottom of page for visual example

--- src/components/Main/ticker.jsx
+++ src/components/Main/ticker.jsx
@@ -1,47 +1,82 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
+import $ from "jquery";
 import Ticker from "react-ticker";
 import PageVisibility from "react-page-visibility";
 
-const makeAPICall = () => {
+const USER_ID = "d359bea4502a44948cc3ee7a77b2afbf";
 
-};
+class Covid19_ticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageIsVisible: false,
+      newsList: []
+    };
+    this.newsTicker = this.newsTicker.bind(this);
+    this.makeAPICall = this.makeAPICall.bind(this);
+    this.getNewsFromAPI = this.getNewsFromAPI.bind(this);
+    this.makeAPICall = this.makeAPICall.bind(this);
+  }
 
-const GetRatesFromAPI = () => {
-  const [rates, setRates] = useState("");
-  useEffect(() => {
-    async function fetchData() {
-      const ratesFromAPI = await makeAPICall();
-      setRates(ratesFromAPI);
-    }
-    fetchData();
-  }, []);
-  // A placeholder is needed, to tell react-ticker, that width and height might have changed
-  // It uses MutationObserver internally
-  return rates ? (
-    <p style={{ whiteSpace: "nowrap" }}>{rates.join(" +++ ")} +++ </p>
-  ) : (
-    <p style={{ visibility: "hidden" }}>Placeholder</p>
-  );
-};
+  componentDidMount() {
+    this.makeAPICall();
+  }
 
-const Covid19_ticker = () => {
-  const [pageIsVisible, setPageIsVisible] = useState(true);
-
-  const handleVisibilityChange = (isVisible) => {
-    setPageIsVisible(isVisible);
+  makeAPICall = () => {
+    $.getJSON(
+      "https://newsapi.org/v2/everything?q=covid19&apiKey=" + `${USER_ID}`
+    )
+    .then(({ results }) => 
+        this.setState({ 
+            newsList: results 
+        }));
   };
 
-  return (
-    <PageVisibility 
-        onChange={ handleVisibilityChange }>
-        { pageIsVisible && 
+  getNewsFromAPI = () => {
+    const [news, setNews] = useState("");
+    useEffect(() => {
+        const newsCall = this.state.newsList;
+      async function fetchData() {
+        const newsFromAPI = newsCall;
+        setNews(newsFromAPI);
+      }
+      fetchData();
+    }, []);
+    // A placeholder is needed, to tell react-ticker, that width and height might have changed
+    // It uses MutationObserver internally
+    return news ? (
+      <p style={{ whiteSpace: "nowrap" }}>{news.join(" +++ ")} +++ </p>
+    ) : (
+      <p style={{ visibility: "hidden" }}>Placeholder</p>
+    );
+  };
+
+  newsTicker = () => {
+    const [pageIsVisible, setPageIsVisible] = useState(true);
+
+    const handleVisibilityChange = (isVisible) => {
+      setPageIsVisible(isVisible);
+    };
+
+    return (
+      <PageVisibility 
+        onChange={handleVisibilityChange}>
+        {pageIsVisible && 
             <Ticker>{() => 
-                <GetRatesFromAPI />
-            }</Ticker>
-        }
-    </PageVisibility>
-  );
-};
+                <this.getNewsFromAPI />
+            }</Ticker>}
+      </PageVisibility>
+    );
+  };
+
+  render() {
+    return (
+        <div>
+            <this.newsTicker />
+        </div>
+    );
+  }
+}
 
 export default Covid19_ticker;
 
